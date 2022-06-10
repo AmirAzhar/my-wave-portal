e590832e11ffac3dbd8f79bb6e9289eea28a252a
Amir Azhar <amir97azhar@gmail.com>
2022-06-11 00:53:43 +0800

    Move functions to library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,26 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
+import {
+  checkIfWalletIsConnected,
+  connectWallet,
+} from "./lib/walletConnection";
+
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
-
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
-      } else {
-        console.log("No authorized account found");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error);
-    }
+  const connectWalletHandler = () => {
+    connectWallet().then((res) => {
+      if (res) setCurrentAccount(res);
+    });
   };
 
   const wave = () => {};
 
   useEffect(() => {
-    checkIfWalletIsConnected();
+    checkIfWalletIsConnected().then((res) => {
+      if (res) setCurrentAccount(res);
+    });
   }, []);
 
   return (
@@ -75,7 +44,7 @@ function App() {
           {!currentAccount && (
             <button
               className="p-2 w-fit bg-blue-500 text-white rounded-md transition duration-300 hover:bg-blue-700"
-              onClick={connectWallet}
+              onClick={connectWalletHandler}
             >
               Connect Wallet
             </button>
